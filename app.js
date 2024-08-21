4ce8dbf69d17f05a6b6faa4a98e7c563d9282737
mast8 <ronald.miranda.rodriguez@gmail.com>
2024-08-21 11:28:15 -0400

    input validation

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 function generateBarcode(){
+
     var barcodeValue = document.getElementById("barcodeValue").value;
     var barcodeType = document.getElementById("barcodeType").value;	
-    var showText = document.getElementById("showText").value;			
-    JsBarcode("#barcode", barcodeValue, {
-        format: barcodeType,
-        displayValue: showText,
-        lineColor: "#24292e",
-        width:2,
-        height:40,	
-        fontSize: 20					
-    });		
+    var showText = document.getElementById("showText").value;		
+
+    if (barcodeValue.trim().length > 0 ){
+        JsBarcode("#barcode", barcodeValue, {
+            format: barcodeType,
+            displayValue: showText,
+            lineColor: "#24292e",
+            width:2,
+            height:40,	
+            fontSize: 20					
+        });		
+    }else alert("input empty")
+   
 }
+
 generateBarcode();
